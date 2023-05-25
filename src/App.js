d6b7c3cfd0b0d207155eb59936312c66ef8725ae
Juan Espinosa Osorio <juankespinoso@gmail.com>
2023-05-25 11:34:31 -0500

    we are working with basic components in the same App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const Person = () => {
+  return (
+    <>
+      <h1>Name: John</h1>
+      <h2>Last Name: Doe</h2>
+      <h2>Age: 30</h2>
+    </>
+  );
+};
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person />
+      <Person />
+      <Person />
     </div>
   );
-}
+};
 
 export default App;
