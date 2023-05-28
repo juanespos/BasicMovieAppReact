722ecb81757da9ad5f737c33980b0a3da1b5500b
Juan Espinosa Osorio <juankespinoso@gmail.com>
2023-05-28 18:36:20 -0500

    We displayed a single movie in App.js. it's static so far

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { useEffect } from "react";
 
+import "./App.css";
+import SearchIcon from "./search.svg";
+
 // API KEY: f9530aea
 
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=f9530aea";
+
+const movie1 = {
+  Title: "Spiderman and Grandma",
+  Year: "2009",
+  imdbID: "tt1433184",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg",
+};
 
 const App = () => {
   const searcMovies = async (title) => {
@@ -18,9 +30,38 @@ const App = () => {
   }, []);
 
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Seach for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img
+              src={
+                movie1.Poster !== "N/A"
+                  ? movie1.Poster
+                  : "https://via.placeholder.com/400"
+              }
+              alt={movie1.Title}
+            />
+          </div>
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
