25d91aad8867aafafab3d84e47644fae90cce7d2
Juan Espinosa Osorio <juankespinoso@gmail.com>
2023-05-28 17:04:05 -0500

    We reach out for a movie API that returns a list depending on a film search. So far we haven't done much visible

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+// API KEY: f9530aea
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=f9530aea";
 
 const App = () => {
+  const searcMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searcMovies("Spiderman");
+  }, []);
+
   return (
     <>
       <h1>App</h1>
